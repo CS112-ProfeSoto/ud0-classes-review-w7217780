b2fcf6f98bcdeb702e0f56221e7f94d1dbd26af1
w7217780 <w7217780@gmail.com>
2025-06-10 08:46:08 +0000

    v1

--- Card.java
+++ Card.java
@@ -5,15 +5,17 @@
  * Class Invariant:
  * - Card value represents the number/letter printed on the card,
  * usually in the corners (A, 2, 3, ..., 9, 10, J, Q, K)
- * - Card value is stored as an integer to make error checking/validation easier,
- * but must be outputted appropriately (1 is A, 11 is J, 12 is Q, 13 is K) for user
+ * - Card value is stored as an integer to make error checking/validation
+ * easier,
+ * but must be outputted appropriately (1 is A, 11 is J, 12 is Q, 13 is K) for
+ * user
  * - Card suit represents one of 4 suits (heart, diamond, club, spade)
  * - Card suit is stored as the unicode char representing the suit,
  * constant variables will be used throughout code for consistency
  * - Whenever value/suit is changed, it must be within the valid values
  *
- * @author ???
- * @version ???
+ * @author Ford Culallad
+ * @version v1.0
  */
 
 /*
@@ -49,16 +51,28 @@
 public class Card {
 
 	/*** CONSTANT VARIABLES ***/
+	public static final int DEFAULT_VALUE = 1;
+	public static final char DEFAULT_SUIT = '♥';
 
+	public static final char HEART = '♥';
+	public static final char DIAMOND = '♦';
+	public static final char CLUB = '♣';
+	public static final char SPADE = '♠';
 
 	/*** INSTANCE VARIABLES ***/
-
+	private int value_ = 0;
+	private char suit_ = ' ';
 
 	/*** CONSTRUCTOR METHODS ***/
 	/**
 	 * Default constructor, builds default card object as: A ♥
 	 */
 
+	public Card()
+	{
+		this.value_ = DEFAULT_VALUE;
+		this.suit_ = DEFAULT_SUIT;
+	}
 
 	/**
 	 * Full constructor builds object with all data for instance variables provided.
@@ -70,6 +84,16 @@ public class Card {
 	 *              spade, or club)
 	 */
 
+	public Card(int value, char suit)
+	{
+		boolean init = setAll(value, suit);
+
+		if (!init)
+		{
+			System.out.println("ERROR : Invalid Constructor Arguments.");
+			System.exit(1);
+		}
+	}
 
 	/**
 	 * Copy constructor builds object with all data from Card object provided. No
@@ -78,6 +102,17 @@ public class Card {
 	 * @param original Card object to be copied
 	 */
 
+	public Card(Card otherCard)
+	{
+		if (otherCard == null)
+		{
+			System.out.println("ERROR : Arugment NULL.");
+			System.exit(1);
+		}
+
+		this.suit_ = otherCard.suit_;
+		this.value_ = otherCard.value_;
+	}
 
 	/*** MUTATOR METHODS (SETTERS) ***/
 	/**
@@ -91,6 +126,18 @@ public class Card {
 	 * @return true if card value is between 1 and 13 (inclusive), false otherwise
 	 */
 
+	public boolean setValue(int value)
+	{
+		if (value >= 1 && value <= 13)
+		{
+			value_ = value;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
 	/**
 	 * Sets suit for card only if valid, otherwise will not change instance
@@ -104,6 +151,29 @@ public class Card {
 	 *         false otherwise
 	 */
 
+	// * + setSuit(suit : char) : boolean
+	public boolean setSuit(char suit)
+	{
+		switch (suit)
+		{
+			case '♥':
+				this.suit_ = suit;
+				break;
+			case '♦':
+				this.suit_ = suit;
+				break;
+			case '♣':
+				this.suit_ = suit;
+				break;
+			case '♠':
+				this.suit_ = suit;
+				break;
+			default:
+				return false;
+		}
+
+		return true;
+	}
 
 	/**
 	 * Sets suit and value for card only if valid, returns boolean representing
@@ -117,6 +187,10 @@ public class Card {
 	 * @return true if card suit AND value are valid, false otherwise
 	 */
 
+	public boolean setAll(int value, char suit)
+	{
+		return (setValue(value) && setSuit(suit));
+	}
 
 	/*** ACCESSOR METHODS (GETTERS) ***/
 	/**
@@ -125,6 +199,10 @@ public class Card {
 	 * @return suit as unicode character for heart, spade, diamond, or club
 	 */
 
+	public char getSuit()
+	{
+		return suit_;
+	}
 
 	/**
 	 * Access numerical value of card (1-13)
@@ -133,6 +211,10 @@ public class Card {
 	 *         see {@link #getPrintValue()})
 	 */
 
+	public int getValue()
+	{
+		return value_;
+	}
 
 	/**
 	 * Access value of card as seen by user (A, 2-10, J, Q, K) that would be printed
@@ -142,6 +224,22 @@ public class Card {
 	 *         value 1-13 (see {@link #getValue()})
 	 */
 
+	public String getPrintValue()
+	{
+		switch (value_)
+		{
+			case 1:
+				return "A";
+			case 11:
+				return "J";
+			case 12:
+				return "Q";
+			case 13:
+				return "K";
+			default:
+				return Integer.toString(value_);
+		}
+	}
 
 	/**
 	 * Access ASCII art version of card data, each line separated by newline
@@ -150,6 +248,25 @@ public class Card {
 	 * @return String containing ASCII art with card suit and card print value
 	 */
 
+	public String getPrintCard()
+	{
+		String art = "";
+
+		art += "--------\n";
+		art += "| " + this.suit_ + "  " + this.suit_ + " |\n";
+		if (value_ < 10)
+		{
+			art += "|  " + Integer.toString(value_) + " " + "  |\n";
+		}
+		else
+		{
+			art += "|  " + Integer.toString(value_) + " " + " |\n";
+		}
+		art += "| " + this.suit_ + "  " + this.suit_ + " |\n";
+		art += "--------";
+
+		return art;
+	}
 
 	/*** OTHER REQUIRED METHODS ***/
 	/**
@@ -159,6 +276,21 @@ public class Card {
 	 * @return String containing (print) value and suit, separated by a space
 	 */
 
+	public String toString()
+	{
+		if (value_ == 0 && suit_ == ' ')
+		{
+			return "0 ";
+		}
+		else if (value_ == 1)
+		{
+			return "A" + suit_;
+		}
+		else
+		{
+			return Integer.toString(value_) + suit_;
+		}
+	}
 
 	/**
 	 * Checking for equality of Card objects, all instance variables exactly equal
@@ -170,12 +302,23 @@ public class Card {
 	 *         exactly equal to each other
 	 */
 
+	public boolean equals(Card otherCard)
+	{
+		if (otherCard == null)
+		{
+			return false;
+		}
+
+		return this.value_ == otherCard.value_ && this.suit_ == otherCard.suit_;
+	}
 
 	/*** EXTRA METHODS ***/
 	/**
 	 * Prints card ASCII art to console (see {@link #getPrintCard()})
 	 */
 
-
-
+	public void printCard()
+	{
+		System.out.print(getPrintCard());
+	}
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@
  * Driver program that creates standard 52-card deck (as Card array)
  * and prints out each card in deck.
  *
- * @author ???
- * @version ???
+ * @author Ford Culallad
+ * @version v1.0
  */
 
 public class Main {
@@ -23,7 +23,25 @@ public class Main {
 
 		/*** DRIVER PROGRAM ***/
 		//1. Generate 52 card deck into Card array
+		Card[] deck = new Card[52];
+
+		int index = 0;
+		char[] suits = { ' ', '♥', '♦', '♣', '♠' };
+
+		for (int suit = 1; suit <= 4; suit++)
+		{
+			for (int value = 1; value <= 13; value++)
+			{
+				deck[index] = new Card(value, suits[suit]);
+				index++;
+			}
+		}
 
 		//2. Print deck
+		for(Card card : deck)
+		{
+			card.printCard();
+			System.out.print("\n\n");
+		}
 	}
 }
